d8ea5800c89b282d34aee3cf4c21fa1e4125ce43
salsabilladheawan4 <salsabilla24ti@mahasiswa.pcr.ac.id>
2026-04-01 09:37:09 +0700

    First commit: Pertemuan 3

---
+++ src/pertemuan 3/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./HelloWorld";
+
+createRoot(document.getElementById("root"))
+    .render(
+        <div>
+            <HelloWorld/>
+        </div>
+    )
